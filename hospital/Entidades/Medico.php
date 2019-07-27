56b6311ec3dbe58e846b26e6bd33f71f981646fa
Jhonatancs <jdc_santos@outlook.com>
2019-07-27 20:13:23 -0300

    mexendo na entidade Medico

--- hospital/Entidades/Medico.php
+++ hospital/Entidades/Medico.php
@@ -5,7 +5,7 @@
 	class Medico extends Pessoa {
 
 		/**
-		 * @var string
+		 * @var int
 		 */
 		private $CRM;
 		
@@ -31,9 +31,51 @@
 		 * @method Getters e Setters
 		 */
 		public function getCRM(){ return $this->CRM; }
-		public function setCRM($crm){ $this->CRM = $crm; }
+		public function setCRM(int $crm){ $this->CRM = $crm; }
 		public function getEspecialidades(){ return $this->especialidades; }
 		public function setEspecialidade(Especialidade $e){ $this->especialidades[] = $e; }
 		public function getVlHora(){ return $this->vlHora; }
 		public function setVlHora(double $vlHora){ $this->vlHora = $vlHora; }
+
+		/**
+		 * @param Medico $medico medico a ser cadastrado
+		 * @return bool
+		 */
+		public function cadastrar(Medico $medico){
+
+		}
+
+		/**
+		 * @param Medico $medico medico a ser alterado
+		 * @return bool
+		 */
+		public function alterar(Medico $medico){
+
+		}
+
+		/**
+		 * @param Medico $medico medico a ser excluido
+		 * @return bool
+		 */
+		public function excluir(Medico $medico){
+
+		}
+
+		/**
+		 * @param int $CRM CRM a ser consultado
+		 * @return Medico
+		 */
+		public function consultar(int $crm){
+
+		}
+
+		/**
+		 * Como o PHP nao suporta sobrecarga, eu precisarei declarar 1 outro
+		 * nome para este metodo.
+		 * @param string $nome consulta medicos com este nome
+		 * @return array[Medico]
+		 */
+		public function consultarLista($nome){
+
+		}
 	}
